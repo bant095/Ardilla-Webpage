9bb5858d60883b46ed34eae62aa8d7e385a3fc1f
Abolaji Ayodeji <abolajiayodeji93@gmail.com>
2023-07-09 19:45:03 +0100

    I updated the code

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+//NAVBAR
 let navbar = document.querySelector(".navbar");
 let menu = document.querySelector("#menu-icon");
 
 document.querySelector("#menu-icon").onclick = () => {
   navbar.classList.toggle("active");
   menu.classList.remove("active");
+};
+
+//Window on scroll
+window.onscroll = () => {
+  menu.classList.remove("menu-icon");
+  navbar.classList.remove("active");
 };
